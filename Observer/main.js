bd7cb14b3eb3af0c50285410ed1a67a729130933
Eric Zhao <eric.ye.zhao@gmail.com>
2017-11-12 21:47:31 +1000

    Setup Observer pattern

---
+++ Observer/main.js
@@ -0,0 +1,26 @@
+var Task = require('./task');
+
+var notificationService = function(){
+    var message = 'Notification ';
+    this.update = function(task){
+        console.log(message = task.user + ' for task' + task.name);
+    }
+};
+
+var loggingService = function(){
+    var message = 'Logging ';
+    this.update = function(task){
+        console.log(message = task.user + ' for task' + task.name);
+    }
+};
+
+var auditingService = function(){
+    var message = 'Auditing ';
+    this.update = function(task){
+        console.log(message = task.user + ' for task' + task.name);
+    }
+};
+
+var task1 = new Task({name:'create a demo for constrcutors', user:'Jon'});
+
+task1.save();
